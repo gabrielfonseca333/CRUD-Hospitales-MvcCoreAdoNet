a8768196651869acc9206b2f0cad0a60308f5938
gabrielfonseca <gabrielfonsecag@gmail.com>
2025-02-03 13:48:04 +0100

    Add project files.

---
+++ MvcCoreAdoNet/Controllers/HospitalesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+using MvcCoreAdoNet.Repositories;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HospitalesController : Controller
+    {
+
+        private RepositoryHospital repo;
+
+        public HospitalesController()
+        {
+            this.repo = new RepositoryHospital();
+        }
+
+        public IActionResult Index()
+        {
+            List<Hospital> hospitales = this.repo.GetHospitales();
+            return View(hospitales);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Hospital hospital)
+        {
+            this.repo.CreateHospital
+                (
+                hospital.IdHospital,
+                hospital.Nombre,
+                hospital.Direccion,
+                hospital.Telefono,
+                hospital.Camas);
+
+            ViewData["MENSAJE"] = "Hospital Creado!";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Hospital hospital)
+        {
+
+            this.repo.EditHospital
+                (
+                hospital.IdHospital,
+                hospital.Nombre,
+                hospital.Direccion,
+                hospital.Telefono,
+                hospital.Camas);
+
+            ViewData["MENSAJE"] = "Hospital editado!";
+            return View(hospital);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            this.repo.DeleteHospital(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
